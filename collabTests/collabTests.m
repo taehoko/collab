c0e502195739b682b4b5674d4abba581c9f35437
tko <tko@tko-macbookpro.roam.corp.google.com>
2014-07-17 11:42:40 -0700

    Added parent directory

---
+++ collabTests/collabTests.m
@@ -0,0 +1,34 @@
+//
+//  collabTests.m
+//  collabTests
+//
+//  Created by Taeho Ko on 7/13/14.
+//  Copyright (c) 2014 google. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface collabTests : XCTestCase
+
+@end
+
+@implementation collabTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
